13a8fb508fb51091a236e7af5da5d1dc29fb96f3
Ayush Kishor <58591510+AyushKishor@users.noreply.github.com>
2021-04-13 18:32:20 +0530

    Uploaded Initial Files

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const BodyParser = require("body-parser");
+const https = require("https")
+const request = require("request");
+
+const app = express();
+app.use(BodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/",function(req,res)
+{
+	const url = "https://api.nasa.gov/planetary/apod?api_key=12EdxT6zsvdIbIJWnCPR94cuUayLEygwswykZNg3";
+
+	https.get(url, function(response)
+	{
+		response.on("data",function(data)
+		{
+			const ImageData = JSON.parse(data);
+			var ImageUrl = "";
+			ImageUrl = ImageData.url;
+			const explanation = ImageData.explanation;
+			res.render("index",
+			{
+				ImageUrl: ImageUrl,
+				explanation: explanation
+			});
+		});
+		
+	});
+});
+
+
+app.listen(process.env.PORT || 3000,function(req,res)
+{
+	console.log("Server is running");
+});
